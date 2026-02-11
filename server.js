0462387b6e2286ea57d6fcae70d77b5fd75c9d6a
Ahmed Raza <ahmedraza10122006@gmail.com>
2026-02-12 02:48:49 +0500

    Vercel Installed

---
+++ server.js
@@ -0,0 +1,17 @@
+// server.js
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+import { ensureAccessToken } from "./config/twitch.js";
+import { getBulkUserStats } from "./controllers/twitchController.js";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/health", (req, res) => res.sendStatus(200));
+app.post("/user/bulk", ensureAccessToken, getBulkUserStats);
+
+// EXPORT instead of app.listen()
+export default app;
